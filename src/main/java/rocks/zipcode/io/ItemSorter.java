eee326adc6124d328765232b4b802dcaee7d87d3
Szeng89 <shijinz898@gmail.com>
2024-04-10 00:00:34 -0400

    Part 3 done, found easier solution

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,34 @@
 package rocks.zipcode.io;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+    Item[] items;
+
     public ItemSorter(Item[] items) {
+        this.items = items;
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+
+//        for (int i = 0; i < this.items.length -1; i++) {
+//            for (int j = i + i; j < this.items.length; j++) {
+//                if (comparator.compare(this.items[i], this.items[j]) > 0) {
+//                    Item tempItem = this.items[i];
+//                    this.items[i] = this.items[j];
+//                    this.items[j] = tempItem;
+//                }
+//            }
+//        }
+//        return this.items;
+
+                Arrays.sort(items, comparator);
+                return items;
+
+
     }
 }
